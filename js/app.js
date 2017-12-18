989db40086bf7f50cde2f0924261eed363c8def9
MariacristinaOrtiz <mari_d17@hotmail.com>
2017-12-17 20:01:43 -0500

    añade primer y segundo modal

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
-//Probando jQuery!
-$(document).ready(function () {
+// Probando jQuery!
+$(document).ready(function() {
   console.log($('h1').text());
-})
+});
+$(document).ready(function() {
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
